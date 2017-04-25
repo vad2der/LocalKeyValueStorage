39efaee2e2fdab9a6ba0982d0d05d4efdffd2e07
vad2der <vad.deryabin@gmail.com>
2017-04-25 11:11:14 -0600

    created simple storage with add, list, remove, get capabilities storing in a json file

---
+++ index.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+'use strict';
+
+let fs = require('fs');
+
+let storageName = "storage.json";
+
+let saveToStorage = (object) => {
+	fs.writeFile(storageName, JSON.stringify(object, null, 2), function(error){
+		if(error){console.log(error);}
+	});
+}
+
+let getAllEntries = () => {
+	if(fs.existsSync(storageName)){		
+		return JSON.parse(fs.readFileSync(storageName));
+	}else{return {};}
+}
+
+let getEntryByKey = (key) => {
+	let storage = getAllEntries();
+	let entry = {};
+	entry[key] = storage[key];	
+	if(storage.hasOwnProperty(key)){
+		return entry;
+	}else{
+		return "No such key, value pair in the storage";
+	}
+}
+
+let removeEntryByKey = (key) => {
+	let storage = getAllEntries();
+	let entry = {};
+	if(storage.hasOwnProperty(key)){
+		entry[key] = storage[key];
+		delete storage[key];
+		saveToStorage(storage);
+		return JSON.stringify(entry)+' is deleted';
+	}else{
+		return "No such key, value pair in the storage";
+	}
+}
+
+let addEntry = (k, v) => {
+	let entry = {};
+	entry[k] = v;
+	let storage = {};
+	if(fs.existsSync(storageName)){
+		storage = getAllEntries();		
+	}
+	storage[k] = v;	
+	saveToStorage(storage);
+	console.log('Entry '+JSON.stringify(entry)+' added to storage');
+}
+
+//action argument [add, list, get, remove, test]
+let arg1 = process.argv[2];
+
+if (arg1){
+	if(arg1 == "add"){
+		try{
+			let k = process.argv[3];
+			let v = process.argv[4];
+			(k && v) ? addEntry(k, v) : console.log("format is 'storage add key value'");
+		}catch(e){
+			console.log(e);			
+		}		
+	}else if(arg1 == "list"){
+		console.log(JSON.stringify(getAllEntries(), null, 2));
+	}else if(arg1 == "get"){
+		(process.argv[3]) ? console.log(getEntryByKey(process.argv[3])) : console.log("Enter the key to look for");
+	}else if(arg1 == "remove"){
+		(process.argv[3]) ? console.log(removeEntryByKey(process.argv[3])) : console.log("Enter the key for the entry to delete");
+	}else if(arg1 == "?" || arg1 == "-h"){
+		const info = "\r\nAvailable commands:\r\n\r\n"+
+					"store list              (gives the the object with all entries)\r\n"+
+					"store add key value     (adding to the store key, value pair)\r\n"+
+					"store get key           (returns an entry with requiested key)\r\n"+
+					"store remove key        (removes entry with requested key from the storage)\r\n"+
+					"store ? <-h>            (available commands)"
+		console.log(info);
+	}else{
+		console.log("No action for the argument '"+arg1+"'. Try 'store ?' command");
+	}
+
+}
